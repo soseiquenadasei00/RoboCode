78df178c4704752608360712a81a6ce3fec5a241
Pedro Henrique do Vale Saldanha <56972373+soseiquenadasei00@users.noreply.github.com>
2020-10-30 14:09:19 +0000

    Tutorial do bot básico em C# para o RoboCode

---
+++ BotSimples(RoboCode).cs
@@ -0,0 +1,38 @@
+﻿// Access to standard .NET System
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// Access to the public Robocode API
+using Robocode;
+
+namespace RobocodeExample
+{
+    // The name of your robot is MyRobot, and the robot type is Robot
+    class MyRobot : Robot
+    {
+        // The main method of your robot containing robot logics
+        public override void Run()
+        {
+            // Here we turn the robot to point upwards, and move the gun 90 degrees
+            TurnLeft(Heading - 90);
+            TurnGunRight(90);
+
+            // Infinite loop making sure this robot runs till the end of the battle round
+            while (true)
+            {
+                // Move our robot 5000 pixels ahead
+                Ahead(5000);
+                // Turn the robot 90 degrees
+                TurnRight(90);
+            }
+        }
+
+        // Robot event handler, when the robot sees another robot
+        public override void OnScannedRobot(ScannedRobotEvent e)
+        {
+            // We fire the gun with bullet power = 1
+            Fire(1);
+        }
+    }
+}
